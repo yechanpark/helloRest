3da99fbf6a4734f10515a4863ee7d183bf73f9d6
yechanpark <qkrdpcks0419@naver.com>
2017-06-11 05:21:00 +0900

    comments 추가 및 들여쓰기

--- src/main/java/kr/ac/hansung/exception/UserDuplicatedException.java
+++ src/main/java/kr/ac/hansung/exception/UserDuplicatedException.java
@@ -1,5 +1,8 @@
 package kr.ac.hansung.exception;
 
+// 일종의 Model 역할을 하며 실제 Exception Handling은
+// @ExceptionHander(UserDuplicatedExceptio.class) Annotation을 달아놓은 메서드에서 수행
+// Serialization을 하지 않는다.
 public class UserDuplicatedException extends RuntimeException {
 
 	// Class의 버전을 나타낸다.
